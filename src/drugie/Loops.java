ef16d4a13fa89046de76fcacfb4e5281a230e509
Adamm91 <adam.marcinkowski91@gmail.com>
2018-02-21 20:19:57 +0100

    LoopFor

---
+++ src/drugie/Loops.java
@@ -0,0 +1,28 @@
+package drugie;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert number of elements to multiply");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int number = scanner.nextInt();
+
+            if (number >= 5 && number <= 20) {
+                System.out.println((i + 1) + ". Pow (" + number + ") = " + (number * number));
+            } else {
+                System.out.println("Number not supported. Upgrade to full version.");
+            }
+        }
+        System.out.println("End of the program");
+
+
+
+    }
+
+}
