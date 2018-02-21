4d9ff9f626d128301f70c94c0b45caab0817adda
Adamm91 <adam.marcinkowski91@gmail.com>
2018-02-21 21:00:56 +0100

    Exercises

--- src/drugie/Loops.java
+++ src/drugie/Loops.java
@@ -5,6 +5,73 @@ import java.util.Scanner;
 public class Loops {
     public static void main(String[] args) {
 
+        /*
+           1. wyswietlic kolejne n liczb * 2 (2, 4, 6...)
+           2. wyswietlic tylko parzyste liczby od 1 do n
+           3. wyswietlic tylko liczby podzielne przez 3
+           4. wyswietlic n liter "a"
+           *. obliczyc sume n liczb ( 1 + 2 + 3 + 4 + ... )
+
+        */
+
+        // 1. wyswietlic kolejne n liczb * 2 (2, 4, 6...)
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("zad. 1");
+        int n = scanner.nextInt();
+        int number = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println(number);
+            number += 2;
+        }
+
+        // 2. wyswietlic tylko parzyste liczby od 1 do m
+        System.out.println("zad. 2");
+        n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+
+        // 3. wyswietlic tylko liczby podzielne przez 3
+
+        System.out.println("zad. 3");
+        n = scanner.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            if (i != 0 && i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        //  4. wyswietlic n liter "a"
+
+        System.out.println("zad. 4");
+        int p = scanner.nextInt();
+
+        for (int i = 0; i < p; i++) {
+            System.out.println("a");
+        }
+
+        // *. obliczyc sume n liczb ( 1 + 2 + 3 + 4 + ... )
+
+        System.out.println("zad. *");
+        int r = scanner.nextInt();
+        int suma = 0;
+        for (int i = 1; i <= r; i++) {
+            suma += i;
+
+        }System.out.println(suma);
+
+
+
+
+
+        // forLoopExample();
+
+    }
+
+    private static void forLoopExample() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Insert number of elements to multiply");
@@ -20,9 +87,6 @@ public class Loops {
             }
         }
         System.out.println("End of the program");
-
-
-
     }
 
 }
